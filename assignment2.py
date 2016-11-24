0021c5b4e6670b79ed1271d6d26fc886018ecedb
Lama <lamahamadeh1985@hotmail.com>
2016-11-24 22:59:47 +0000

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# TODO: Load up the 'tutorial.csv' dataset
+#
+# .. your code here ..
+df=pd.read_csv('/Users/lamahamadeh/Downloads/Modules/DAT210x-master/Module2/Datasets/tutorial.csv')
+print(df)
+
+
+# TODO: Print the results of the .describe() method
+#
+# .. your code here ..
+print(df.describe())
+
+
+# TODO: Figure out which indexing method you need to
+# use in order to index your dataframe with: [2:4,'col3']
+# And print the results
+#
+# .. your code here ..
+print(df.loc[2:4,'col3'])
+
