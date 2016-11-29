b9d294a0be9e1618ae77a874c27cb3de944350f9
Lama <lamahamadeh1985@hotmail.com>
2016-11-29 11:43:32 +0000

    Update Parallel_Coordinates.py

--- Parallel_Coordinates.py
+++ Parallel_Coordinates.py
@@ -19,13 +19,13 @@ matplotlib.style.use('ggplot')
 
 
 
-x=np.arange(0,math.pi*2,math.pi/40)
+x=np.arange(0,np.pi*2,np.pi/40)
 #print(x)
 #print(math.pi)
 
 
-data = {'-x':x, 
-        'x':-x,
+data = {'x':x, 
+        '-x':-x,
         'Sin(x)':np.sin(x),
         'Cos(x)': np.cos(x),
         'Tan(x)': np.tan(x),
@@ -35,11 +35,44 @@ data = {'-x':x,
 df = pd.DataFrame(data, index=None) #without the index there will be an index column starting from 0
 print(df) 
 
-'''
-plt.scatter(x,Sinx)
-plt.show()
-'''
+#Playing with plots and subplots before going to parallel coordinates
+plt.figure(1)
+plt.subplot(311)
+plt.plot(x,np.sin(x),'b', linewidth=2.0)
+plt.xlabel('x')
+plt.ylabel('Sin(x)')
+plt.grid(False)
+plt.xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi],
+           ['$0$', r'$\frac{\pi}{2}$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'])
 
+plt.subplot(312)
+plt.plot(x,np.cos(x),'r', linewidth=2.0)
+plt.xlabel('x')
+plt.ylabel('Cos(x)')
+plt.grid(False)
+plt.xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi],
+           ['$0$', r'$\frac{\pi}{2}$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'])
+
+plt.subplot(313)
+plt.plot(x,np.tan(x),'g', linewidth=2.0)
+plt.xlabel('x')
+plt.ylabel('Tan(x)')
+plt.grid(False)
+plt.xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi],
+           ['$0$', r'$\frac{\pi}{2}$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'])
+
+plt.show()
+
+
+plt.plot(x,np.sin(x),'b',x,np.cos(x),'r', linewidth=2.0)
+plt.xlabel('x')
+plt.ylabel('Cos(x) (in red) and Sin(x) (in blue)')
+plt.xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi],
+           ['$0$', r'$\frac{\pi}{2}$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'])
+
+plt.show()
+
+#Here we go!
 plt.figure()
 parallel_coordinates(df,'x',alpha= 0.4)
 plt.show()
