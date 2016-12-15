3b84e57c4a7dbdddeb9faea84bfce58775ce10f3
Lama <lamahamadeh1985@hotmail.com>
2016-12-15 11:05:57 +0000

    Update assignment1.py

--- Module 5/assignment1.py
+++ Module 5/assignment1.py
@@ -3,12 +3,10 @@
 #
 # .. your code here ..
 
-import numpy as np
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-
 
 
 matplotlib.style.use('ggplot') # Look Pretty
@@ -28,7 +26,7 @@ matplotlib.style.use('ggplot') # Look Pretty
 #
 # .. your code here ..
 
-df=pd.read_csv('/Users/lamahamadeh/Downloads/Modules/DAT210x-master/Module5/Datasets/Crimes_-_2001_to_present.csv', index_col=0)
+df1=pd.read_csv('/Users/lamahamadeh/Downloads/Modules/DAT210x-master/Module5/Datasets/Crimes_-_2001_to_present.csv', index_col=0)
 
 
 #
@@ -36,70 +34,59 @@ df=pd.read_csv('/Users/lamahamadeh/Downloads/Modules/DAT210x-master/Module5/Data
 #
 # .. your code here ..
 
-df= df.dropna(axis = 0, how = 'any')
-print (df)
+df1.dropna(axis = 0, how = 'any', inplace = True)
 #
 # TODO: Print out the dtypes of your dset
 #
 # .. your code here ..
 
-print(df.dtypes)
+print(df1.dtypes)
 #
 # Coerce the 'Date' feature (which is currently a string object) into real date,
 # and confirm by re-printing the dtypes. NOTE: This is a slow process...
 #
 # .. your code here ..
 
-df.Date = pd.to_datetime(df.Date) # Converts the entries in the 'Date' column to datetime64[ns]
-print (df.dtypes)
+df1.Date = pd.to_datetime(df1.Date) # Converts the entries in the 'Date' column to datetime64[ns]
+print (df1.dtypes)
 
-#
-# TODO: Filter df so that you're only looking at Longitude and Latitude,
+
+  
+def doKMeans(df):
+    # TODO: Filter df so that you're only looking at Longitude and Latitude,
 # since the remaining columns aren't really applicable for this purpose.
 #
 # .. your code here ..
 
-df1=df[['Longitude','Latitude']]
-
-print (df1)
-
-
-
-
+    df=df[['Longitude','Latitude']]
+    
 #
 # INFO: Plot your data with a '.' marker, with 0.3 alpha at the Longitude,
 # and Latitude locations in your dataset. Longitude = x, Latitude = y
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.set_xlabel('Longitude')
-ax.set_ylabel('Latitude')
-ax.scatter(df.Longitude, df.Longitude, marker='.', alpha=0.3)
-plt.show()
-  
-
-  #
-  # TODO: Use K-Means to try and find seven cluster centers in this df.
-  #
-  # .. your code here ..
-  
-def doKMeans(df):
-    kmeans_model = KMeans(n_clusters=7)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(x=df.Longitude, y=df.Latitude, marker='.', alpha=0.5, s = 30)
+   
+#
+# TODO: Use K-Means to try and find seven cluster centers in this df.
+#
+# .. your code here ..
+    kmeans_model = KMeans(n_clusters=7, init = 'random', n_init = 60, max_iter = 360, random_state = 43)
     kmeans_model.fit(df)
-    KMeans(copy_x=True, init='k-means++', max_iter=300, n_clusters=7, n_init=10,
-    n_jobs=1, precompute_distances='auto', random_state=None, tol=0.0001,
-    verbose=0)
-    labels = kmeans_model.predict(df)
-    
+    labels = kmeans_model.predict(df)  
+  
   # INFO: Print and plot the centroids...
   
     centroids = kmeans_model.cluster_centers_
-    ax.scatter(centroids[:,0], centroids[:,1], marker='x', c='red', alpha=0.5, linewidths=3, s=169)
+    ax.scatter(centroids[:,0], centroids[:,1], marker='x', c='red', alpha=0.9, linewidths=3, s=250)
     print (centroids)
- 
+    
+
 
 # INFO: Print & Plot your data
-doKMeans(df)
-
+doKMeans(df1)
+plt.title("For all dataframe dates")
+plt.show()
 
 #
 # TODO: Filter out the data so that it only contains samples that have
@@ -108,10 +95,9 @@ doKMeans(df)
 #
 # .. your code here ..
 
-df1 = df[df.Date > '2011-01-01']
+df2 = df1[df1.Date > '2011-01-01']
 
 # INFO: Print & Plot your data
-doKMeans(df1)
-plt.title("Dates limited to 2011 and later")
-
+doKMeans(df2)
+plt.title("Dates limited to 2011-01-01 and later")
 plt.show()
