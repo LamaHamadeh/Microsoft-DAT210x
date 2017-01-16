f4871a0915d51241312a40cd72ef87c6059be026
Lama <lamahamadeh1985@hotmail.com>
2017-01-16 20:31:51 +0000

    Update PythonBasics.py

--- PythonBasics.py
+++ PythonBasics.py
@@ -166,7 +166,20 @@ df.iloc[:,:2]# Select the first 2 columns
 
 print(df) 
 #----------------
+#matplotlib and plot cusomisation
+import numpy as np
+import matplotlib.pyplot as plt
 
+x = np.linspace(0, 10, 20)
+y1 = x**2.0
+y2 = x**1.5
+plt.plot(x, y1, 'bo-', linewidth = 2, markersize = 12, label = 'First')
+plt.plot(x, y2, 'gs-', linewidth = 2, markersize = 12, label = 'Second')
+plt.xlabel('$X$') #matplotlibe support LaTex typsetting 
+plt.ylabel('$Y$')
+plt.axis([-0.5, 10.5, -5, 105])#organise the axes
+plt.legend(loc = 'upper left')#create a legend based on the labels specified earlier for each plot and define its position 
+plt.savefig('myplot.pdf')#sae the figure as a .pdf file (you can save it as .png file as well).
 #------------------------------------------------------------------------------
 
 
