3ac1c1554cf56576dcb7ae99b1459e7ae882c49a
Lama <lamahamadeh1985@hotmail.com>
2017-02-17 18:32:17 +0000

    Update assignment5.py

--- Module 6/assignment5.py
+++ Module 6/assignment5.py
@@ -43,8 +43,6 @@ def num_missing(x):
 print "Missing values per column:"
 print X.apply(num_missing, axis=0) #axis=0 defines that function is to be applied on each column
 
-print X.shape
-
 # TODO: Go ahead and drop any row with a nan
 #
 # .. your code here ..
@@ -60,13 +58,16 @@ print "After dropping all rows with any NaNs, shape of X is:", X.shape
 #
 # .. your code here ..
 
+y = X['classification'].copy()
+X.drop('classification', axis = 1, inplace = True)
+y = y.map({'e': 0, 'p': 1})
 
 #
 # TODO: Encode the entire dataset using dummies
 #
 # .. your code here ..
 
-
+X = pd.get_dummies(X, columns = ['cap-shape', 'cap-surface', 'cap-color', 'bruises?', 'odor', 'gill-attachment', 'gill-spacing', 'gill-size', 'gill-color', 'stalk-shape', 'stalk-root', 'stalk-surface-above-ring', 'stalk-surface-below-ring', 'stalk-color-above-ring', 'stalk-color-below-ring', 'veil-type', 'veil-color', 'ring-number', 'ring-type', 'spore-print-color', 'population', 'habitat'])
 # 
 # TODO: Split your data into test / train sets
 # Your test size can be 30% with random_state 7
@@ -74,21 +75,25 @@ print "After dropping all rows with any NaNs, shape of X is:", X.shape
 #
 # .. your code here ..
 
-
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30, random_state = 7)
 
 #
 # TODO: Create an DT classifier. No need to set any parameters
 #
 # .. your code here ..
+
 from sklearn import tree
 DT = tree.DecisionTreeClassifier()
-DT.fit(X,y)
- 
+
 #
 # TODO: train the classifier on the training data / labels:
 # TODO: score the classifier on the testing data / labels:
 #
 # .. your code here ..
+
+DT.fit(X_train, y_train)
+score = DT.score(X_test, y_test)
 print "High-Dimensionality Score: ", round((score*100), 3)
 
 
@@ -99,4 +104,9 @@ print "High-Dimensionality Score: ", round((score*100), 3)
 #
 # .. your code here ..
 
+tree.export_graphviz(DT, out_file = 'tree.dot', feature_names = X.columns)
 
+import subprocess
+subprocess.call(['dot', '-Tpdf', 'tree.dot', '-o' 'tree.pdf'])
+
+     
